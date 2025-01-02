efcce3dcac1fef44c31aeb58b5c24aac2773f25d
terra <544708735@qq.com>
2025-01-02 15:29:04 +0800

    adapter

--- 2_structural_patterns/1_adapter/adapter.h
+++ 2_structural_patterns/1_adapter/adapter.h
@@ -1,6 +1,106 @@
 #ifndef __ADAPTER_H__
 #define __ADAPTER_H__
 
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <memory>
+
+/**
+ * target method 
+ */
+class target
+{
+public:
+  virtual std::string request() const
+  {
+    return "target: the default target behavior.";
+  }
+};
+
+class adaptee
+{
+public:
+  std::string reverse_request() const 
+  {
+    return ".roivaheb eetpada tluafed eht :eetpada";
+  };
+};
+
+/**
+ * adapter override target request
+ */
+class adapter : public target,public adaptee
+{
+public:
+  std::string request() const override 
+  {
+    std::string old_str = reverse_request();
+    std::reverse(old_str.begin(),old_str.end());
+    return "adapter behavior:" + old_str;
+  }
+};
+
+/**
+ * old client code
+ */
+class client  
+{
+public:
+  static void client_code(const std::shared_ptr<target> param)
+  {
+    std::cout << param->request() << std::endl;
+  }
+};
+
+
+/**
+ * 或者如果有一些方法不兼容，可以通过一个适配器来增加一个兼容的接口
+ */
+class tools_a
+{
+public:
+  void get_tools_a()
+  {
+    std::cout << "use tools a. \n";
+  }
+};
+
+class tools_b
+{
+public:
+  void get_tools_b()
+  {
+    std::cout << "use tools b. \n";
+  }
+};
+
+class tools_adapter : public tools_b
+{
+public:
+  tools_adapter(tools_a a): tools_a_(a){}
+
+  void get_tools_b()
+  {
+    tools_a_.get_tools_a();
+    tools_a_to_tools_b();
+    std::cout << "use tools b. \n";
+  }
+private:
+  void tools_a_to_tools_b()
+  {
+    std::cout << "change tools a to tools b. \n";
+  }
+
+  tools_a     tools_a_;
+};
+
+
+
+
+
+
+
 
 
 
--- 2_structural_patterns/1_adapter/main.cpp
+++ 2_structural_patterns/1_adapter/main.cpp
@@ -1,12 +1,24 @@
 /**
  * 适配器模式：我们想要的接口和现有的接口不同时，我们可以在现有的接口上构造一个适配器来支持想要的接口。
- * 这么一想，工厂模式，是不是也是某种程度上的适配器模式，提供统一的对外接口，不同的适配器，构造不同的产品
  */
 
 #include "adapter.h"
 
+#include <memory>
+
 int main()
 {
+  std::shared_ptr<target> my_target(new target);
+  std::shared_ptr<target> my_adapter(new adapter);
+  std::shared_ptr<adaptee> my_adaptee(new adaptee);
+  client::client_code(my_target);
+  std::cout << my_adaptee->reverse_request() << std::endl;
+  client::client_code(my_adapter);
   
+  tools_a a;
+  // tools_b b;
+  tools_adapter tools_adapter(a);
+  tools_adapter.get_tools_b();
+
   return 0;
 }
