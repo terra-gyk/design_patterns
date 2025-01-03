648a23d8096cdc8ce387b1156af31f3047eb5497
terra <544708735@qq.com>
2025-01-03 14:47:23 +0800

    decorator

--- 2_structural_patterns/4_decorator/decorator.h
+++ 2_structural_patterns/4_decorator/decorator.h
@@ -1,8 +1,66 @@
 #ifndef __DECORATOR_H__
 #define __DECORATOR_H__
 
+#include <iostream>
+#include <string>
+#include <memory>
 
+class hero
+{
+public:
+  virtual void show_skill() = 0;
+};
 
+class run_hero : public hero
+{
+public:
+  void show_skill() override
+  {
+    run_skill();
+  }
+private:
+  void run_skill()
+  {
+    std::cout << "hero can run.\n";
+  }
+};
 
+class fly_hero : public hero
+{
+public:
+  fly_hero(std::shared_ptr<hero> hero) : base_hero_(hero) {}
+  void show_skill() override 
+  {
+    base_hero_->show_skill();
+    run_skill();
+  }
+
+private:
+  void run_skill()
+  {
+    std::cout << "hero can fly.\n";
+  }
+
+  std::shared_ptr<hero>  base_hero_;
+};
+
+class laser_hero : public hero
+{
+public:
+  laser_hero(std::shared_ptr<hero> hero) : base_hero_(hero) {}
+  void show_skill() override 
+  {
+    base_hero_->show_skill();
+    run_skill();
+  }
+
+private:
+  void run_skill()
+  {
+    std::cout << "hero can shoot laser.\n";
+  }
+
+  std::shared_ptr<hero>  base_hero_;
+};
 
 #endif // __DECORATOR_H__
--- 2_structural_patterns/4_decorator/main.cpp
+++ 2_structural_patterns/4_decorator/main.cpp
@@ -1,12 +1,22 @@
 /**
  * 装饰器模式是一种结构型设计模式， 允许你通过将对象放入包含行为的特殊封装对象中来为原对象绑定新的行为。
- * 
+ * Component：被装饰的主体，定义了一个对象接口，可以给这些对象增加装饰功能；
+ * ConcreteComponent：具体的对象，继承于Component；
+ * Decorator：装饰者，继承于Component并从Component类的外部完成对Component类功能的扩充，并且Component类并不需要知道装饰者的存在；
  */
 
 #include "decorator.h"
 
 int main()
 {
+  auto run_hero_1 = std::make_shared<run_hero>();
+  auto fly_hero_1 = std::make_shared<fly_hero>(run_hero_1);
+  auto laser_hero_1 = std::make_shared<laser_hero>(fly_hero_1);
 
+  run_hero_1->show_skill();
+  std::cout << "hero upgrade.\n"; 
+  fly_hero_1->show_skill();
+  std::cout << "hero upgrade.\n"; 
+  laser_hero_1->show_skill();
   return 0;
 }
