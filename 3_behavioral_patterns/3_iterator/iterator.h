f1c9ba30a796022fa38ef19945e95c7b01820edf
terra <544708735@qq.com>
2025-01-16 10:53:37 +0800

    iterator

--- 3_behavioral_patterns/3_iterator/iterator.h
+++ 3_behavioral_patterns/3_iterator/iterator.h
@@ -1,8 +1,80 @@
 #ifndef  __ITERATOR_H__
 #define  __ITERATOR_H__
 
+/**
+ * Iterator Design Pattern
+ *
+ * Intent: Lets you traverse elements of a collection without exposing its
+ * underlying representation (list, stack, tree, etc.).
+ */
+
+#include <iostream>
+#include <string>
 #include <vector>
 
-std::vector<int>::iterator it;
+template<typename P>
+class base_iterator
+{
+public:
+  base_iterator(P* p) : data_point_(p){}
+
+  bool operator==(base_iterator that)
+  {
+    return this->data_point_ == that.data_point_;
+  }
+
+  bool operator!=(base_iterator that)
+  {
+    return !(this->data_point_ == that.data_point_);
+  }
+
+  base_iterator& operator++(int)
+  {
+    this->data_point_ = this->data_point_ + 1;
+    return *this;
+  }
+
+  P& operator*() const 
+  {
+    return *data_point_;
+  }
+
+  friend std::ostream& operator<<(std::ostream& os, const base_iterator<P>& iter)
+  {
+    os << *iter.data_point_;
+    return os;
+  }
+
+private:
+  P*     data_point_;
+}; 
+
+template<typename T, int size = 20>
+class container 
+{
+public:
+  using iterator = base_iterator<T>;
+
+  // 这个构造实现很粗糙，不是很建议这么用
+  container(T* data)
+  {
+    for(int index = 0; index < size; index++)
+    {
+      data_[index] = data[index];
+    }
+  };
+
+  iterator begin()
+  {
+    return iterator{data_};
+  };
+
+  iterator end()
+  {
+    return iterator{data_ + size};
+  };
+private:
+  T            data_[size];
+};
 
 #endif // __ITERATOR_H__
--- 3_behavioral_patterns/3_iterator/mian.cpp
+++ 3_behavioral_patterns/3_iterator/mian.cpp
@@ -1,5 +1,6 @@
 /**
  * 迭代器模式是一种行为设计模式， 让你能在不暴露集合底层表现形式 （列表、 栈和树等） 的情况下遍历集合中所有的元素。
+ * 可以参考 stl 中迭代器的实现，迭代器一定是基于某个容器的
  */
 
 #include "iterator.h"
@@ -7,6 +8,14 @@
 
 int main()
 {
+  int num[10] = {1,2,3,4,5,6,7,8,9,10};
+  container<int,3> arr{num};
+  container<int>::iterator ii = arr.begin();
+
+  for( ; ii != arr.end(); ii++)
+  {
+    std::cout <<  *ii << std::endl;
+  }
 
   return 0;
 }
