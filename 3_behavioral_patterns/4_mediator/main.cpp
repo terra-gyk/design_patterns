0be23d86c4a2d30b0ef522b982b5f9ed4dc112b3
terra <544708735@qq.com>
2025-01-16 14:40:20 +0800

    mediator

--- 3_behavioral_patterns/4_mediator/main.cpp
+++ 3_behavioral_patterns/4_mediator/main.cpp
@@ -1,6 +1,5 @@
 /**
- * 迭代器模式是一种行为设计模式， 让你能在不暴露集合底层表现形式 （列表、 栈和树等） 的情况下遍历集合中所有的元素。
- * 可以参考 stl 中迭代器的实现，迭代器一定是基于某个容器的
+ * 迭中介者模式是一种行为设计模式， 能让你减少对象之间混乱无序的依赖关系。 该模式会限制对象之间的直接交互， 迫使它们通过一个中介者对象进行合作。
  */
 
 #include "mediator.h"
@@ -8,6 +7,13 @@
 
 int main()
 {
+  auto component_a_1 = std::make_shared<component_a>();
+  auto component_b_1 = std::make_shared<component_b>();
+  auto mediator_1 = std::make_shared<sample_mediator>(component_a_1,component_b_1);
+  component_a_1->set_mediator(mediator_1);
+  component_b_1->set_mediator(mediator_1);
+  component_a_1->do_some();
+  component_b_1->do_some();
 
   return 0;
 }
--- 3_behavioral_patterns/4_mediator/mediator.h
+++ 3_behavioral_patterns/4_mediator/mediator.h
@@ -1,6 +1,91 @@
 #ifndef __MEDIATOR_H__
 #define __MEDIATOR_H__
 
+#include <iostream>
+#include <memory>
+
+enum class mediator_event { A, B, C, D };
+
+class base_component;
+
+class mediator
+{
+public:
+  virtual void notify(mediator_event event) = 0;
+};
+
+class base_component
+{
+public:
+  void set_mediator(std::shared_ptr<mediator> mediator) 
+  { 
+    mediator_ = mediator;
+  }
+
+protected:
+  std::shared_ptr<mediator>  mediator_;
+};
+
+class component_a : public base_component 
+{
+public:
+  void do_some()
+  {
+    std::cout << "component a do some. \n";
+    this->mediator_->notify(mediator_event::B);
+  }
+
+  void some_for_b()
+  {
+    std::cout << "component a do some for b. \n";
+  }
+};
+
+class component_b : public base_component 
+{
+public:
+  void do_some()
+  {
+    std::cout << "component b do some. \n";
+    this->mediator_->notify(mediator_event::A);
+  }
+
+  void some_for_a()
+  {
+    std::cout << "component b do some for a. \n";
+  }
+};
+
+class sample_mediator : public mediator
+{
+public:
+  sample_mediator(std::shared_ptr<component_a> a, std::shared_ptr<component_b> b):component_a_(a),component_b_(b){}
+  void notify(mediator_event event)
+  {
+    switch (event) 
+    {
+      case mediator_event::A:
+        std::cout << "notify a. \n";
+        component_a_->some_for_b();
+        break;
+      case mediator_event::B:
+        std::cout << "notify b. \n";
+        component_b_->some_for_a();
+        break;
+      case mediator_event::C:
+        break;
+      case mediator_event::D:
+        break;
+      default:
+        std::cout << "event not support in sample mediator.\n";
+        break;
+    }
+  }
+private:
+  std::shared_ptr<component_a>  component_a_;
+  std::shared_ptr<component_b>  component_b_;
+};
+
 
 
 #endif // __MEDIATOR_H__
